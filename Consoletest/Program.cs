32a6f64acc21c4cd7d13c17ac60bee4fceb558e3
Alireza <Alireza.najjarii@gmail.com>
2023-04-04 17:58:40 +0330

    Add project files.

---
+++ Consoletest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoletest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 35; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ selenium test/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace selenium_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.leader.ir/fa");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            var archive = driver.FindElement(By.XPath("/html/body/footer/nav[3]/ul/li[5]/h6/a"));
+            archive.Click();
+            var section = driver.FindElement(By.XPath("/html/body/main/div[1]/section[1]/main/div[1]/ul/li[2]"));
+            section.Click();
+            //var decade = driver.FindElements(By.XPath("/html/body/main/div[1]/section[1]/main/div[2]/ul/li"));
+            var speech = driver.FindElement(By.XPath($"/html/body/main/div[1]/section[2]/main/ul[2]/li/div[2]/h6/a[2]"));
+            speech.Click();
+            var title = driver.FindElement(By.ClassName("btitr"));
+            var articleBody = driver.FindElement(By.Id("details"));
+            var date = driver.FindElement(By.XPath("/html/body/main/div[1]/section/main/article[1]/div[1]/time/h6"));
+
+            var heading = title.Text;
+            var body = articleBody.Text;
+            var name = date.Text;
+            name = new string(name.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            string filePath = @"C:\Users\MEMO\Desktop\test";
+            string fileName = $"{name}.txt";
+            string fullPath = Path.Combine(filePath, fileName);
+            File.WriteAllText(fullPath, heading + body);
+
+
+            driver.Navigate().Back();
+            //for (int i = 1; i <= 4; i++)
+            //{
+            //    var year = driver.FindElement(By.XPath($"/html/body/main/div[1]/section[1]/main/div[2]/ul/li[{i}]"));
+            //    year.Click();
+
+            //    for (int j = 1; j <= 12; j++)
+            //    {
+            //        var month = driver.FindElement(By.XPath($"/html/body/main/div[1]/section[1]/main/div[3]/ul/li[{j}]"));
+            //        month.Click();
+            //        var speechNumber = driver.FindElements(By.XPath("/html/body/main/div[1]/section[2]/main/ul"));
+            //        for (int k = 0; k < speechNumber.Count; k++)
+            //        {
+            //            var speech = driver.FindElement(By.XPath($"/html/body/main/div[1]/section[2]/main/ul[{k}]/li/div[2]/h6/a[2]"));
+            //            speech.Click();
+
+            //        }
+
+            //    }
+            //}
+
+
+        }
+    }
+
+}
